a7339e44e3ecc3f63d9a7e7ff57d53b3ebd50db0
OleksandrVasylchuk <73894374+OleksandrVasylchuk@users.noreply.github.com>
2021-05-09 02:46:10 +0300

    розібрався з кнопкою

--- color.js
+++ color.js
@@ -14,6 +14,7 @@ const randomIntegerFromInterval = (min, max) => {
 };
 
 const refs = {
+  span: document.querySelector('span'),
   body: document.querySelector('body'),
   start: document.querySelector('[data-action="start"]'),
   stop: document.querySelector('[data-action="stop"]')
@@ -22,17 +23,22 @@ const refs = {
 refs.start.addEventListener('click', startColorChange)
 
 function changeColore(){
-  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0,5)];
+  refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)];
+ 
 };
 
 
 function startColorChange() {
   refs.start.disabled = "disabled";
+  refs.start.classList.add('hidden');
+  //  document.getElementById('MyButton').style.visibility = 'hidden';
   refs.start.removeEventListener("click", startColorChange)
   const intervalINid = setInterval(changeColore, 1000);
   refs.stop.addEventListener("click", stopColorChange)
   function stopColorChange() {
     refs.start.disabled = "";
+    refs.start.classList.remove('hidden');
+    //  document.getElementById('MyButton').style.visibility = 'hidden';
     clearInterval(intervalINid);
     refs.start.addEventListener("click", startColorChange)
   }
